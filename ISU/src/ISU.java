2ec971aecf1b18b3b274410109bfd53b77437541
cyruschoisy <77631273+cyruschoisy@users.noreply.github.com>
2022-05-31 08:42:23 -0400

    Add files via upload

---
+++ ISU/src/ISU.java
@@ -0,0 +1,20 @@
+import javafx.event.ActionEvent;
+
+public class ISU {
+
+    // Constructor
+    public ISU () {
+
+    } 
+
+    public void actionPerformed (ActionEvent event) {
+        
+    }
+
+    public void newGame ()
+	{
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
