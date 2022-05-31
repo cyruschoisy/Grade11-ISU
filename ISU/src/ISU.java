a291c0f2ba775d8103fb84b5949f23c604d2e807
cyruschoisy <77631273+cyruschoisy@users.noreply.github.com>
2022-05-31 09:16:33 -0400

    Initializing start up screen

--- ISU/src/ISU.java
+++ ISU/src/ISU.java
@@ -1,10 +1,25 @@
 import javafx.event.ActionEvent;
+import java.awt.Dimension;
+import javax.swing.*;
 
 public class ISU {
 
     // Constructor
     public ISU () {
+        frame = new JFrame ("Basic JFrame Example");
+        frame.setPreferredSize(new Dimension(200, 200));
+        frame.setLocation(200, 200);
 
+        myPanel = new JPanel ();
+        label = new JLabel ("Hello World!");
+        button = new JButton ("Hide");
+
+        myPanel.add (label);
+        myPanel.add (button);
+
+        frame.add (myPanel);
+        frame.pack ();
+        frame.setVisible (true);
     } 
 
     public void actionPerformed (ActionEvent event) {
