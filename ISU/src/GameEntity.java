4fd32da6abf05cd0f5a0012a4c9df20ca69a514a
Christian <cfisla@itmatter.com>
2022-06-01 11:34:19 -0400

    changed comments

--- ISU/src/GameEntity.java
+++ ISU/src/GameEntity.java
@@ -1,12 +1,14 @@
 public class GameEntity {
 
-    //Initialize variables, default values for now
+    //Initialize variables
     public static int health;
     public static int speed;
 
     //Main method
     public GameEntity(int health, int speed) {
         //Object code here
+
+        
     }
 
 }
