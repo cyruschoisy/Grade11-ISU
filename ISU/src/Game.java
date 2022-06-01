1fac43d7209342dd6feea0318bc37ddefccb1dbd
Christian <cfisla@itmatter.com>
2022-06-01 12:28:25 -0400

    added animation methods
    I still need to decide whether I want to draw the animations in the method or in ISU.java

--- ISU/src/Game.java
+++ ISU/src/Game.java
@@ -1,16 +1,22 @@
 public class Game {
 
+    //Initialize variables
+    public static int map = 1;
+
+    //Main method
     public static void Main (String[] args) {
 
         //Initialize 2D Array
-        int[][] board;
+        //20 rows, 15 columns
+        //Each box should be 40 x 40 pixels large
+        int[][] board = new int[15][20];
 
         //Set the health and speed of the enemies
         GameEntity.health = 1;
         GameEntity.speed = 100;
 
         //Instantiate the object
-        GameEntity enemy = new GameEntity(1, 100);
+        GameEntity enemy = new GameEntity();
 
     }
 
--- ISU/src/GameEntity.java
+++ ISU/src/GameEntity.java
@@ -4,11 +4,34 @@ public class GameEntity {
     public static int health;
     public static int speed;
 
-    //Main method
-    public GameEntity(int health, int speed) {
-        //Object code here
+    public void start() {
+        //Add the image onto the screen ("spawn it in")
+        //Some code
 
-        
+        //Alter animation path for different maps
+        if (Game.map == 1) {
+            animationPathOne(GameEntity.speed);
+        } else if (Game.map == 2) {
+            animationPathTwo(GameEntity.speed);
+        } else if (Game.map == 3) {
+            animationPathThree(GameEntity.speed);
+        }
     }
 
+    public static void animationPathOne(int speed) {
+        //Describe the path of the entity
+    }
+
+    public static void animationPathTwo(int speed) {
+        //Describe the path of the entity
+    }
+
+    public static void animationPathThree(int speed) {
+        //Describe the path of the entity
+    }
+
+    //Main method
+    public GameEntity() {
+        start();
+    }
 }
