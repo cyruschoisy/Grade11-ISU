d29a002f738eced373ed2822b24bd407bacfd007
Christian <cfisla@itmatter.com>
2022-06-01 09:20:06 -0400

    added new object game entity

---
+++ ISU/src/GameEntity.java
@@ -0,0 +1,10 @@
+public class GameEntity {
+
+    int x = 5;
+
+    public static void Init(String[] args) {
+        GameEntity Enemy = new GameEntity();
+        System.out.println(Enemy.x);
+    }
+
+}
