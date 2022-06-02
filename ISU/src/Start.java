26f9f488007b90c96147d58977180915c614e58a
cyruschoisy <77631273+cyruschoisy@users.noreply.github.com>
2022-06-02 16:48:45 -0400

    Meeting with Wong
    Figured out that the problem lies with intellij, and our code does work on eclipse

--- ISU/src/Start.java
+++ ISU/src/Start.java
@@ -3,67 +3,60 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class Start extends JPanel {
-        public static JFrame frame;
-        public static Graphics g;
+    public static JFrame frame;
+    public static Graphics g;
 
-        JPanel myPanel;
-        JPanel buttons;
+    JPanel myPanel;
+    JPanel buttons;
 
-        JButton startButton;
-        JButton aboutUs;
-        JButton howTo;
+    JButton startButton;
+    JButton aboutUs;
+    JButton howTo;
 
-        JLabel bgd;
-        ImageIcon bgdImage;
-        String background = "/media/firstMap.png";
+    JLabel bgd;
+    ImageIcon bgdImage;
+    String background = "/media/firstMap.png";
 
+    GameEntity enemy;
 
-        GameEntity enemy;
-        // Constructor
-    public Start () {
-
-
-        setLayout(new BorderLayout());
-
+    // Constructor
+    public Start() {
+        setLayout (new BorderLayout());
         buttons = new JPanel();
-        buttons.setLayout(new BoxLayout(buttons, BoxLayout.PAGE_AXIS));
+        buttons.setLayout (new BoxLayout(buttons, BoxLayout.PAGE_AXIS));
 
         startButton = new JButton("Start");
         aboutUs = new JButton("About us");
         howTo = new JButton("How to play");
 
-  //      bgd = new JLabel ("/media/towerDefence.png");
+        bgd = new JLabel ("media/towerDefence.png");
 
+        buttons.add (startButton);
+        buttons.add (aboutUs);
+        buttons.add (howTo);
 
-        buttons.add(startButton);
-        buttons.add(aboutUs);
-        buttons.add(howTo);
-//
-      //  add (BorderLayout.NORTH, bgd);
-        add (BorderLayout.SOUTH, buttons);
-
-////////////////////////////////
-
+        //  add (BorderLayout.NORTH, bgd);
+        add(BorderLayout.SOUTH, buttons);
 
         //Instantiate the object
-         enemy = new GameEntity();
-
+        enemy = new GameEntity();
     }
 
-    public void paintComponent (Graphics g ) {
+    public void paintComponent (Graphics g) {
         super.paintComponent (g);
-        Image blah = Toolkit.getDefaultToolkit().getImage("/media/enemyOne.png");
-        g.drawImage (blah, 100,100, this);
+        Image blah = Toolkit.getDefaultToolkit().getImage("media/enemyOne.png");
+        g.drawImage(blah, 100, 100, this);
 
-
+        System.out.print ("HI");
     }
+
     public void actionPerformed(ActionEvent event) {
 
     }
 
-    public static void main(String[] args) {
-        frame = new JFrame("Basic JFrame Example");
-        frame.setPreferredSize(new Dimension(800, 600));
+    public static void main (String[] args) {
+        frame = new JFrame ("Basic JFrame Example");
+        frame.setPreferredSize (new Dimension (800, 600));
         frame.setLocation(0, 0);
 
         Start myPanel = new Start ();
@@ -71,7 +64,7 @@ public class Start extends JPanel {
         frame.pack ();
         frame.setVisible (true);
 
-       // Game.Main();
+        // Game.Main();
     }
 }
 
