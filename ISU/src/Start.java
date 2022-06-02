b03a19f01e8295f7c7b4d07fdfe7cb18e1ae6974
Christian <cfisla@itmatter.com>
2022-06-02 09:48:50 -0400

    .

--- ISU/src/Start.java
+++ ISU/src/Start.java
@@ -67,7 +67,6 @@ public class Start extends JPanel {
         frame.setLocation(0, 0);
 
         Start myPanel = new Start ();
-        frame.setUndecorated(true);
         frame.add (myPanel);
         frame.pack ();
         frame.setVisible (true);
