1247d1ea1e0883f143c348329500f4b9a85ad5f6
cyruschoisy <77631273+cyruschoisy@users.noreply.github.com>
2022-06-02 09:08:02 -0400

    adding exit button

--- ISU/src/Start.java
+++ ISU/src/Start.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-public class Start {
+public class Start extends Component {
         public static JFrame frame;
         public static Graphics g;
 
@@ -12,10 +12,12 @@ public class Start {
         JButton startButton;
         JButton aboutUs;
         JButton howTo;
+        JButton exit;
 
         JLabel bgd;
         ImageIcon bgdImage;
         String background = "/media/firstMap.png";
+        Image startbgd;
 
         // Constructor
     public Start () {
@@ -32,25 +34,50 @@ public class Start {
         startButton = new JButton("Start");
         aboutUs = new JButton("About us");
         howTo = new JButton("How to play");
+        exit = new JButton ("Exit");
 
-        bgd = new JLabel ("/media/towerDefence.png");
+        MediaTracker tracker = new MediaTracker (this);
+        startbgd = Toolkit.getDefaultToolkit ().getImage ("/media/towerDefence.png");
+        tracker.addImage (startbgd, 0);
 
+        //  Wait until all of the images are loaded
+        try
+        {
+            tracker.waitForAll ();
+        }
+        catch (InterruptedException e)
+        {
+        }
 
         buttons.add(startButton);
         buttons.add(aboutUs);
         buttons.add(howTo);
+        buttons.add(exit);
 
         myPanel.add (bgd);
         myPanel.add (buttons);
 
-        frame.setUndecorated(true);
+        frame.setUndecorated(false);
         frame.add (myPanel);
         frame.pack ();
         frame.setVisible (true);
+
+        setFocusable (true); // Need this to set the focus to the panel in order to add the keyListener
+//        addKeyListener (this);
+//        addMouseListener (this);
     }
 
     public void actionPerformed(ActionEvent event) {
+        String eventName = event.getActionCommand ();
+        if (eventName.equals ("New"))
+        {
+//            newGame ();
+        }
 
+        else if (eventName.equals ("Exit"))
+        {
+            System.exit (0);
+        }
     }
 
     public static void main(String[] args) {
